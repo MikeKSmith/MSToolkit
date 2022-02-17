5974ba79c29c1e24b8b3084117ff3e6a44644efb
jluo0015 <jluo0015@student.monash.edu>
2022-02-17 22:03:05 +1100

    Document the removeDirectories function

--- R/removeDirectories.r
+++ R/removeDirectories.r
@@ -1,12 +1,12 @@
 #' Remove directories
-#'
+#' 
 #' This function removes directories containing data for the current scenario.
-#'
+#' 
 #' dirNames must be a character vector.  The function will attempt to remove
 #' each of the directories identified in the dirNames vector from within the
 #' "workingPath" directory If method is "Internal", the DataStore is emptied
 #' and no directories are modified
-#'
+#' 
 #' @param dirNames (Optional) A vector of directories to remove from the
 #' "workingPath" directory.  Default is to remove "ReplicateData",
 #' "MicroEvaluation" and "MacroEvaluation" directories
@@ -17,29 +17,30 @@
 #' default)
 #' @return A logical vector the same length as the dirNames, indicating whether
 #' or not the corresponding directories were successfully removed
-#' @author Francisco Gochez
-#' @keywords IO
+#' @author Mike K Smith \email{mstoolkit@@googlemail.com}
+#' @keywords data
 #' @examples
-#'
+#' 
 #'   \dontrun{
 #'     createDirectories(dirNames = c("ReplicateData", "MicroEvaluation"))
 #'     removeDirectories(dirNames = c("ReplicateData", "MicroEvaluation"))
 #'   }
-#'
-removeDirectories <- function(
-  dirNames = c("ReplicateData", "MicroEvaluation", "MacroEvaluation"),
-  workingPath = getwd(),
-  method = getEctdDataMethod()
+#' 
+removeDirectories <- function(  
+  dirNames = c("ReplicateData", "MicroEvaluation", "MacroEvaluation"), 
+                         #@ A vector containing the full names of the directories to be removed
+  workingPath = getwd(), #@ Directory in which to remove directories
+  method = getEctdDataMethod()   #@ Data Storage Method to use
 ) {
-  ##############################################################################
+  ###############################################################################
   # Mango Solutions, Chippenham SN14 0SQ 2006
   # removeDirectories.R 20/06/2007 15:28:29 BST 2007 @445 /Internet Time/
-  # Author: Francisco Gochez
-  ##############################################################################
-  # DESCRIPTION: Tries to remove named subdirectories.  Returns a logical vector
+  # Author: Francisco
+  ###############################################################################
+  # DESCRIPTION: Tries to remove named subdirectories.  Returns a logical vector 
   # representing the success or failure of directory removal
   # KEYWORDS: IO
-  ##############################################################################
+  ###############################################################################
 
   # Quit if the list is too short
   if(!length(dirNames)) ectdStop("No directories to remove")
@@ -54,13 +55,9 @@ removeDirectories <- function(
   if (length(dirNames) > 1) {
 
 	  checkTrue <- c()
-
-	  for (i in 1:length(dirNames)){
-	    checkTrue[i] <- removeDirectories(dirNames[i],
-	                                      workingPath = workingPath,
-	                                      method = method)
-	  }
-
+	  
+	  for (i in 1:length(dirNames)) checkTrue[i] <- removeDirectories(dirNames[i], workingPath = workingPath, method = method)
+	  
 	  return(checkTrue)
   }
   else {
