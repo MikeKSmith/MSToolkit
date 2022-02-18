7524b5ef98011bec4b78598eea3b977217d026c5
jluo0015 <jluo0015@student.monash.edu>
2022-02-19 00:23:07 +1100

    Finish editing the test file for compilesummary function

--- tests/testthat/test-compilesummary.R
+++ tests/testthat/test-compilesummary.R
@@ -26,3 +26,78 @@ test_that("test.compileSummary.failure", {
 
   unlink(tf, recursive = TRUE)
 })
+
+
+test_that("test.compileSummary.R", {
+
+  ## copy files accross
+  dir.create(cpdir <- file.path(tempdir(), "compileSummary.R"))
+  dir.create(file.path(cpdir, "MacroEvaluation"))
+  dir.create(file.path(cpdir, "MicroEvaluation"))
+  for(i in 1:5){
+    file.copy(file.path(compileSummary.datapath, "MicroEvaluation", sprintf("micro%04d.csv", i)),
+              file.path(cpdir, "MicroEvaluation"))
+    file.copy(file.path(compileSummary.datapath, "MacroEvaluation", sprintf("macro%04d.csv", i)),
+              file.path(cpdir, "MacroEvaluation"))
+  }
+
+  compileSummary("Micro", workingPath = cpdir)
+  compileSummary("Macro", workingPath = cpdir)
+  expectedMicroData <- read.csv(file.path(compileSummary.datapath, "microSummary.csv"))
+  newMicroData <- read.csv(file.path(compileSummary.datapath, "microSummary.csv"))
+  expect_equal(expectedMicroData, newMicroData,
+               info = "compile micro R")
+  expectedMacroData <- read.csv(file.path(compileSummary.datapath, "macroSummary.csv"))
+  newMacroData <- read.csv(file.path(compileSummary.datapath, "macroSummary.csv"))
+  expect_equal(expectedMicroData, newMicroData,
+               info = "compile macro R")
+
+  unlink(cpdir, recursive = TRUE)
+
+})
+
+
+test_that("test.compileSummary.missing", {
+
+  ## copy files accross
+  dir.create(cpdir <- file.path(tempdir(), "compileSummary.missing"))
+  dir.create(file.path(cpdir, "MacroEvaluation"))
+  dir.create(file.path(cpdir, "MicroEvaluation"))
+  for(i in 1:5){
+    file.copy(file.path(compileSummary.datapath, "MicroEvaluation", sprintf("micro%04d.csv", i)),
+              file.path(cpdir, "MicroEvaluation"))
+    file.copy(file.path(compileSummary.datapath, "MacroEvaluation", sprintf("macro%04d.csv", i)),
+              file.path(cpdir, "MacroEvaluation"))
+  }
+
+  expect_error(compileSummary("Micro", replicates = 1:10, workingPath = cpdir),
+               info = "not compile when missing files (micro)")
+  expect_error(compileSummary("Macro", replicates = 1:10, workingPath = cpdir),
+               info = "not compile when missing files (macro)")
+
+  unlink(cpdir, recursive = TRUE)
+
+})
+
+
+test_that("test.compileSummary.holes.R", {
+
+  ## copy files accross
+  dir.create(cpdir <- file.path(tempdir(), "compileSummary.missing"))
+  dir.create(file.path(cpdir, "MacroEvaluation"))
+  dir.create(file.path(cpdir, "MicroEvaluation"))
+  for(i in 1:5){
+    file.copy(file.path(compileSummary2.datapath, "MicroEvaluation", sprintf("micro%04d.csv", i)),
+              file.path(cpdir, "MicroEvaluation"))
+    file.copy(file.path(compileSummary2.datapath, "MacroEvaluation", sprintf("macro%04d.csv", i)),
+              file.path(cpdir, "MacroEvaluation"))
+  }
+
+  expect_error(compileSummary("Micro", workingPath = cpdir),
+               info = "not all columns the same")
+  expect_error(compileSummary("Macro", workingPath = cpdir),
+               info = "not all columns the same")
+
+  unlink(cpdir, recursive = TRUE)
+
+})
