fb43a0348cbb8fb1cb815db238450216e3633971
jluo0015 <jluo0015@student.monash.edu>
2022-03-11 01:13:09 +1100

    Add header = TRUE into read.csv()

--- data-raw/internalData.R
+++ data-raw/internalData.R
@@ -6,14 +6,13 @@ library(tidyverse)
 library(here)
 
 # create a R script to access the data for the example of macroEvaluation function
-microData <- read.csv(here("tests/testthat/data/macroEvaluation/micro0001.csv"))
+microData <- read.csv(here("tests/testthat/data/macroEvaluation/micro0001.csv"), header = TRUE)
 
 # Create this dataset to provide the data sample for testing the writeData/readData function.
 ReplicateSample <- read.csv(here("tests/testthat/testdata.datastorage/ReplicateSample.csv"))
 
 # Create this dataset to provide test data for testing readData function (test.readData.CSV)
 microSummary <- read.csv(here("tests/testthat/testdata.datastorage/microSummary.csv"))
-
 
 # Create the testdata.datastorage folder path
 testdata_datastorage_path <- here("tests/testthat/testdata.datastorage")
