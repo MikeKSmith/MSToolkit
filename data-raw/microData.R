dd7b605406dd0d855b0ff9452333c4d5ef53d804
jluo0015 <jluo0015@student.monash.edu>
2022-03-10 12:42:45 +1100

    Add a new internal dataset as test data for testing the writeData function

--- data-raw/microData.R
+++ data-raw/microData.R
@@ -1,8 +1,13 @@
 ## code to prepare `microData` dataset goes here
 
+# These codes are used to create the internal data for the function samples or testing
+
 library(tidyverse)
+library(here)
+
 # create a R script to access the data for the example of macroEvaluation function
-microData <- read.csv(here::here("tests/testthat/data/macroEvaluation/micro0001.csv"))
+microData <- read.csv(here("tests/testthat/data/macroEvaluation/micro0001.csv"))
+# Create this dataset to provide the data sample for testing the writeData function.
+ReplicateSample <- read.csv(here("tests/testthat/testdata.datastorage/ReplicateSample.csv"))
 
-
-usethis::use_data(microData, overwrite = TRUE, internal = TRUE)
+usethis::use_data(microData, ReplicateSample, overwrite = TRUE, internal = TRUE)
