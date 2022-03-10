0c2e08c816a7305efcf37286460f6a6888544cd9
jluo0015 <jluo0015@student.monash.edu>
2022-03-10 19:21:49 +1100

    insert the dataset called microSummary into internal data

--- data-raw/internalData.R
+++ data-raw/internalData.R
@@ -1,4 +1,4 @@
-## code to prepare `microData` dataset goes here
+## code to prepare `internalData` dataset goes here
 
 # These codes are used to create the internal data for the function samples or testing
 
@@ -7,7 +7,11 @@ library(here)
 
 # create a R script to access the data for the example of macroEvaluation function
 microData <- read.csv(here("tests/testthat/data/macroEvaluation/micro0001.csv"))
+
 # Create this dataset to provide the data sample for testing the writeData function.
 ReplicateSample <- read.csv(here("tests/testthat/testdata.datastorage/ReplicateSample.csv"))
 
-usethis::use_data(microData, ReplicateSample, overwrite = TRUE, internal = TRUE)
+# Create this dataset to provide test data for testing readData function (test.readData.CSV)
+microSummary <- read.csv(here("tests/testthat/testdata.datastorage/microSummary.csv"))
+
+usethis::use_data(microData, ReplicateSample, microSummary, overwrite = TRUE, internal = TRUE)
